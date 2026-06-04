152d3b3a3ea841d132ac13070fdb06c0739534dd
mshohoudi <mohammad.shohoudimojdehi.1@ens.etsmtl.ca>
2026-06-03 22:26:07 -0400

    test(solveurs): ajout des tests unitaires pour les solveurs analytique et EF
    - Création des tests unitaires pour valider les calculs mécaniques.
    - Vérification des sorties pour le déplacement Y et la contrainte X.
    - Sécurisation des solveurs contre les régressions futures.

--- tests/test_solveurs.py
+++ tests/test_solveurs.py
@@ -1,3 +1,45 @@
 import unittest
-import sys
-import os
+from source.solveur_analytique import SolveurAnalytique
+from source.solveur_ef import SolveurFEA1D
+
+
+class TestSolveurs(unittest.TestCase):
+    def setUp(self):
+        # Paramètres standards pour les tests
+        self.L = 1.0
+        self.b = 0.1
+        self.h = 0.1
+        self.E = 200e9
+        self.alpha = 1.2e-5
+
+        self.force_kn = 15.0
+        self.delta_t = 80.0
+
+        # Initialisation des solveurs
+        self.solveur_ana = SolveurAnalytique(self.L, self.b, self.h, self.E, self.alpha)
+        self.solveur_fea = SolveurFEA1D(self.L, self.b, self.h, self.E, self.alpha, nb_elements=20)
+
+    def test_solveur_analytique(self):
+        # Vérification du déplacement
+        deplacement = self.solveur_ana.calculer_deplacement_max(self.force_kn, self.delta_t)
+        self.assertIsNotNone(deplacement)
+        self.assertTrue(deplacement > 0.0, "Déplacement doit être positif")
+
+        # Vérification de la contrainte
+        contrainte = self.solveur_ana.calculer_contrainte_max(self.force_kn, position_x=0.09)
+        self.assertIsNotNone(contrainte)
+        self.assertTrue(contrainte > 0.0, "Contrainte doit être positive")
+
+    def test_solveur_fea(self):
+        # Vérification des résultats FEA
+        deplacement, contrainte = self.solveur_fea.calculer_resultats(self.force_kn, self.delta_t)
+
+        self.assertIsNotNone(deplacement)
+        self.assertTrue(deplacement > 0.0, "Déplacement FEA doit être positif")
+
+        self.assertIsNotNone(contrainte)
+        self.assertTrue(contrainte > 0.0, "Contrainte FEA doit être positive")
+
+
+if __name__ == '__main__':
+    unittest.main()
