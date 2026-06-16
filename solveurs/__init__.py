5b59a0adf06540cb689fc2fff9ac63fd10079d7e
Nallard92 <nicolas.allard.1@ens.etsmtl.ca>
2026-06-16 16:32:58 -0400

    fix : Mise à jour des __init__.py

---
+++ solveurs/__init__.py
@@ -0,0 +1,8 @@
+"""
+Package contenant les solveurs et modules principaux du projet.
+"""
+from .gestion_donnees import *
+from .modele_substitution import *
+from .solveur_analytique import *
+from .solveur_ef import *
+from visualisation import *
